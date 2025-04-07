b9a31847efde04aba529cdabe2733e2c0b34c398
Anish Kumar Thakur <103921473+ak804670@users.noreply.github.com>
2025-04-07 18:23:08 +0530

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,6 +3,7 @@ import '../styles/header.css'
 import CTA from './CTA'
 import me from '../assets/me.png'
 import HeaderSocial from './HeaderSocial'
+import { TypeAnimation } from 'react-type-animation';
 
 const Header = () => {
   return (
@@ -10,7 +11,25 @@ const Header = () => {
       <div className="container header__container">
         <h5>Hello, I am</h5>
         <h1>Anish Kumar</h1>
-        <h5 className="text-light">Full Stack Developer</h5>
+{/*         <h5 className="text-light">Full Stack Developer</h5> */}
+        <h4>
+         <TypeAnimation
+      sequence={[
+        'Full Stack Developer',
+        1500,
+        'Master of Vanilla JS & Beyond',
+        1500,
+        'React + Node Full-Stack Pro',
+        1500,
+        'From Pixels to APIs, I Build It All',
+        1500
+      ]}
+      wrapper="span"
+      speed={50}
+      style={{ fontSize: '2em', display: 'inline-block' }}
+      repeat={Infinity}
+    />
+          </h4>
         <CTA/>
         <HeaderSocial/>
         <div className="me">
