1f9be66e8455bcbf154792cd9dd627f22304a1cd
Anish Kumar Thakur <103921473+ak804670@users.noreply.github.com>
2026-06-02 09:57:10 +0530

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -22,7 +22,7 @@ const About = () => {
             <article className='about__card'>
               <RiAwardFill className='about__icon' />
               <h5>Work Experience</h5>
-              <small>2+ Years</small>
+              <small>2.9+ Years</small>
             </article>
 
             <article className='about__card'>
